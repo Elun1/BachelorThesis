f439bc5a53b5963fb4a3b40f2d3a9a002f528b72
Elun1 <elias.lundmark@outlook.com>
2017-04-28 16:59:31 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+import datetime
+import random
+import runjobs
+import sortlogs
+
+def main():
+    #list of jobs from SpecCPU, please read README.md for info
+    benchmark_list = ['400.perlbench.sh', '401.bzip2.sh', '403.gcc.sh', '410.bwaves.sh', 
+    '429.mcf.sh', '433.milc.sh', '434.zeusmp.sh', '435.gromacs.sh', 
+    '436.cactusADM.sh', '437.leslie3d.sh', '444.namd.sh', '445.gobmk.sh', 
+    '447.dealII.sh', '450.soplex.sh', '453.povray.sh', '456.hmmer.sh', '454.calculix.sh' 
+    '458.sjeng.sh', '459.GemsFDTD.sh', '462.libquantum.sh', '464.h264ref.sh', '465.tonto.sh'
+    '470.lbm.sh', '471.omnetpp.sh', '473.astar.sh', '481.wrf.sh', '482.sphinx3.sh', '483.xalancbmk.sh']
+
+    test_jobs = ['400.jobA.sh', '401.jobB.sh']
+
+    remotehost = '10.0.0.1'
+    create_log_folders()
+    runjobs.single_thread(benchmark_list, remotehost)
+    runjobs.all_combinations(benchmark_list, remotehost)
+
+    totaljoblist = [4, 8, 12, 24]
+
+    counter = 0:
+    while counter < 100:
+        jobs = random.sample(benchmark_list, 2)
+        for totaljobs in totaljobslist:
+            runjobs.all_pairs(jobs, totaljobs, remotehost)
+            runjobs.no_pairs(jobs, totaljobs, remotehost)
+            if totaljobs == 12:
+                runjobs.no_ht(jobs, totaljobs, remotehost)
+
+def create_log_folders():
+
+    #These are the folders I use, add subfolders for different scaling methods
+    root_folder = './logs/'
+    subfolders = ['Runs2_2/', 'Runs2_4/', 'Runs2_6/', 'Runs2_12/', 'AllCombinations', 'SingleThread']
+
+    #Create root and subfolders if they don't exist
+    if not os.path.isfile(root_folder):
+        os.system('mkdir {folder}'.format(folder=root_folder))
+
+    for subfolder in subfolders:
+        folder = root_folder + subfolder
+        if not os.path.isfile(folder):
+            os.system('mkdir {folder}'.format(folder=folder))
+
+if __name__ == '__main__':
+    main()
---
+++ runjobs.py
@@ -0,0 +1,232 @@
+import os
+import time
+import itertools
+
+def start_job(thread, job, log, donejobs):
+
+    donefile = 'done.{job}.{thread}'.format(job=job, thread=thread)
+    taskset = '(/usr/bin/time -f \"Thread {thread}: {job}: %e\" -a -o {log} taskset -c {thread} ./{job};touch {donefile}) &'
+    os.system(taskset.format(thread=thread, job=job, log=log, donefile=donefile))
+
+    print('Running {job} on Thread: {thread}'.format(job=job, thread=thread))
+    if donefile not in donejobs:
+        donejobs.append(donefile)
+
+    return donejobs
+
+def start_job_wo_time(thread, job, done):
+
+    taskset = '(taskset -c {thread} ./{job}; touch {done}) &' 
+    os.system(taskset.format(thread=thread, job=job, done=done))
+    print('Running dummy for {job} on Thread: {thread}'.format(job=job, thread=thread)) 
+
+
+def sync():
+
+    os.system("sync")
+    time.sleep(30)
+
+def scp_log(logdir, logfile, host):
+
+    remote_dir = logdir.replace('./', '')
+    scp_cmd = 'scp {log} {remotehost}:{remotedir}'
+    os.system(scp_cmd.format(log=log, remotehost=remotehost, logdir=logdir))
+
+def single_thread(jobs, remotehost=None):
+
+    #this function doesn't use start_job and check_done 
+    thread = 0
+    logdir = './logs/SingleThread/'
+
+    for job in jobs:
+        logfile = logdir + 'ST_{job}.log'.format(job=job)
+        donefile = 'done.{job}.{thread}'.format(job=job, thread=thread)
+
+        taskset = '(/usr/bin/time -f \"Thread {thread}: {job}: %e\" -a -o {log} taskset -c {thread} ./{job};touch {donefile}) &'
+        os.system(taskset.format(thread=thread, job=job, log=logfile, donefile=donefile))
+
+        while os.path.isfile(donefile) == False:
+            time.sleep(5)
+        os.system('rm {done}'.format(done=donefile))
+
+        sync()
+        if remotehost:
+            scp_log(logdir, logfile, remotehost)
+
+def all_combinations(jobs, remotehost=None):
+
+    thread1 = 0
+    thread2 = 12
+    totaljobs = 2
+    logdir = './logs/AllCombinations'
+    donejobs = []
+
+    for comb in itertools.combinations_with_replacement(jobs, 2):
+        logfile = logdir + '/{jobA}_{jobB}.log'.format(jobA=comb[0], jobB=comb[1])
+        start_job(thread1, comb[0], logfile, donejobs)
+        start_job(thread2, comb[1], logfile, donejobs)
+
+        finishedlist = []
+        finished = False
+        if comb[0] != comb[1]:
+            check_done(donejobs, totaljobs)
+        else:
+            while not finished:
+                time.sleep(5)
+                for donefile in donejobs:
+                    if os.path.isfile(donefile):
+                        finishedlist.append(donefile)
+                    if len(finishedlist) >= totaljobs:
+                        finished = True:
+
+        for donefile in finishedlist:
+            os.system('rm {done}'.format(done=donefile))
+
+        if remotehost:
+            scp_log(logdir, logfile, remotehost)
+        sync()
+
+def all_pairs(jobs, totaljobs, remotehost=None):
+
+    jobs_unique = len(jobs)
+    jobs_instances = totaljobs//jobs_unique
+    assert jobs_unique % 2 == 0
+
+    logdir = './logs/Runs{jobs}_{instances}'.format(jobs=jobs_unique, instances=jobs_instances)
+    logfile = logdir + '/{jobA}_{jobB}_AllPairs.log'.format(jobA=jobs[0], jobB=jobs[1])
+
+    print('Running {jobs}_{instances} All Pairs'.format(jobs=jobs_unique, instances=jobs_instances))
+
+    donejobs = []
+    thread = 0
+
+    for job in jobs:
+        for _ in range(jobs_instances//2):
+            start_job(thread, job, logfile, donejobs)
+            start_job(thread+12, job, logfile, donejobs)
+            thread += 1
+
+    print('All processes are running')
+    check_done(donejobs, totaljobs)
+
+    if remotehost:
+        scp_log(logdir, logfile, remotehost)
+    sync()
+
+def no_pairs(jobs, totaljobs, remotehost=None):
+
+    jobs_unique = len(jobs)
+    jobs_instances = totaljobs//jobs_unique
+    assert jobs_unique % 2 == 0
+
+    logdir = './logs/Runs{jobs}_{instances}'.format(jobs=jobs_unique, instances=jobs_instances)
+    logfile = logdir + '/{jobA}_{jobB}_NoPairs.log'.format(jobA=jobs[0], jobB=jobs[1])
+
+    print('Running {jobs}_{instances} No Pairs'.format(jobs=jobs_unique, instances=jobs_instances))
+    job_split1 = jobs[:jobs_unique//2]
+    job_split1 = jobs[jobs_unique//2:]
+    donejobs = []
+
+    thread = 0
+    for job in job_split1:
+        for _ in range(thread, thread+job_instances):
+            start_job(thread, job, logfile, donejobs)
+
+    thread = 12
+    for job in job_split2:
+        for _ in range(thread, thread+job_instances):
+            start_job(thread, job, logfile, donejobs)
+
+    print("All processes are running")
+    check_done(donejobs, totaljobs)
+
+    if remotehost:
+        scp_log(logdir, logfile, remotehost)
+    sync()
+
+def no_ht(jobs, totaljobs, remotehost=None):
+    jobs_unique = len(jobs)
+    jobs_instances = totaljobs//jobs_unique
+    assert jobs_unique % 2 == 0
+    assert totaljobs <= 12
+
+    logdir = './logs/Runs{jobs}_{instances}'.format(jobs=jobs_unique, instances=jobs_instances)
+    logfile = logdir + '/{jobA}_{jobB}_NoHT.log'.format(jobA=jobs[0], jobB=jobs[1])
+
+    print('Running {jobs}_{instances} without HT'.format(jobs=jobs_unique, instances=jobs_instances))
+
+    donejobs = []
+    thread = 0
+
+    for _ in range(total_jobs//2):
+        start_job(thread, jobs[0], logfile, donejobs)
+        start_job(thread+6, jobs[1], logfile, donejobs)
+        thread += 1
+    print("All processes are running")
+    check_done(donejobs, totaljobs)
+
+    if remotehost:
+        scp_log(logdir, logfile, remotehost)
+    sync()
+
+def check_logs(jobs, totaljobs):
+    #This is bad, and I should feel bad. Also only works if totaljobs == 2
+    jobs_unique = len(jobs)
+    jobs_instances = totaljobs//jobs_unique
+    logdir = './logs/Runs{unique}_{instances}'.format(unique=jobs_unique, instances=jobs_instances)
+    logfile = logdir + '/{jobA}_{jobB}'.format(jobA=jobs[0], jobB=jobs[1])
+
+    allpairs_log = logfile + '_AllPairs'
+    nopairs_log = logfile + '_NoPairs'
+    logexist = 0
+
+    if os.path.isfile(allpairs_log):
+        logexist += 1
+    if os.path.isfile(nopairs_log):
+        logexist += 1
+
+    jobs[0], jobs[1] = jobs[1], jobs[0]
+    logfile = logdir + '/{jobA}_{jobB}'.format(jobA=jobs[0], jobB=jobs[1])
+
+    if os.path.isfile(allpairs_log):
+        logexist += 1
+    if os.path.isfile(nopairs_log):
+        logexist += 1
+
+    return logexist
+
+def check_done(donejobs, totaljobs):
+
+    finishedlist = []
+    finished = False
+
+    while finished = False:
+        time.sleep(2)
+        for donefile in donejobs:
+            if os.path.isfile(donefile):
+                donesplit = donefile.split('.')
+                thread = donesplit[4]
+                indices = [1, 2, 3]
+                job = '.'.join([donesplit[i] for i in indices])
+                if donefile not in finishedlist:
+                    finishedlist.append(donefile)
+                os.system('rm {done}'.format(done=donefile))
+                start_job_wo_time(thread, job, donefile)
+                if len(finishedlist) >= totaljobs:
+                    finished = True:
+
+    print('Done running dummies\nWaiting for existing dummies to finish.')
+
+    finishedlist = []
+    finished = False
+
+    while finished = False:
+        time.sleep(2)
+        for donefile in donejobs:
+            if os.path.isfile(donefile) and donefile not in finishedlist:
+                finishedlist.append(donefile)
+            if len(finishedlist) >= totaljobs:
+                finished = True:
+
+    print('All dummies are done\nCleaning up...\n')
+    os.system('rm done*')
